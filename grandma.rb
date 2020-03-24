e2872e79380308fadbd73ee11a48c202d60d7e5a
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-24 19:20:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,8 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma
+  if lowercare "HUH?! SPEAK UP, SONNY!"
+  elsif "LOVE YOU GRANDMA!" "I LOVE YOU TOO PUMPKIN!""
+  else 
